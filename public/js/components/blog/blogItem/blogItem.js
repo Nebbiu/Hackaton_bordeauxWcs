70e6cd3c2d80cfb93b3d602a76d8dac31805b83c
krm22 <krmouat22@gmail.com>
2016-11-28 12:50:54 +0100

    initial commit of webapp skeleton

---
+++ public/js/components/blog/blogItem/blogItem.js
@@ -0,0 +1,41 @@
+((app) => {
+    app.component('blogItem', {
+        templateUrl: 'js/components/blog/blogItem/blogItem.html',
+        controller: function($stateParams, postsService, $state) {
+
+            postsService.getById($stateParams.id).then((response) => {
+                this.blog = response.data;
+            });
+
+            this.delete = (blog) => {
+              postsService.delete(blog).then((response) => {
+                console.log('Blog deleted');
+                $state.go('blog.list')
+              })
+            };
+
+            this.saveBlogs = (blog) => {
+                  postsService.save(blog).then((res) =>{
+                console.log("Blog saved sucessfully")
+              });
+            }
+
+
+
+            this.resetTodoState = () => {
+                this.blogs.forEach(function(blog) {
+
+                })
+            }
+
+
+            this.editMode = (blog, index) => {
+                this.blog.editMode = true;
+
+                let date = new Date();
+                this.hhmm = (new Date(), 'hh:mm');
+
+            };
+        }
+    }); //dont delete
+})(angular.module('app.blog'))
